f1ec0bcfd8d23da77aae333d7dc43ce7b257a49f
razanalsaad <razan_alsaad@hotmail.com>
2023-10-05 11:52:45 +0300

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by razan on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
